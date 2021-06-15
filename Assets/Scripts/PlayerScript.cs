f8706d9352019f4e2a3e179aeb6d60e63269de17
Gwe2B <gwenael2b@live.fr>
2021-06-15 17:43:27 +0200

    Add of animations
    Animation for Hello

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -13,21 +13,25 @@ namespace Assets.Scripts.Player
         [Range(1, 10)] public float fallMultiplier = 2.5f;
 
         private InputManager inputManager;
+        private Animator animator;
         private new Rigidbody rigidbody;
 
         private void Start()
         {
             inputManager = GetComponent<InputManager>();
             rigidbody = GetComponent<Rigidbody>();
+            animator = GetComponent<Animator>();
         }
 
         void Update()
         {
 
-            if (inputManager.hasJumpInput && Mathf.Approximately(0, rigidbody.velocity.y))
+            /*if (inputManager.hasJumpInput && Mathf.Approximately(0, rigidbody.velocity.y))
             { rigidbody.velocity = Vector3.up * jumpVelocity; }
             else if (rigidbody.velocity.y < -0.1)
-            { rigidbody.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime; }
+            { rigidbody.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime; }*/
+            if (inputManager.hasJumpInput)
+                animator.SetTrigger("SayHello");
 
             transform.Translate(new Vector3(inputManager.moveInput.x, 0, inputManager.moveInput.y) * moveSpeed * Time.deltaTime);
         }
